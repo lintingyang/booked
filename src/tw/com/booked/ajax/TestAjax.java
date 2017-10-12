d52dba6aed4509a621be7e8ff25a806ad8245bbd
002093 <002093@ASD52A.aegon.com.tw>
2017-10-12 16:42:42 +0800

    test2

--- src/tw/com/booked/ajax/TestAjax.java
+++ src/tw/com/booked/ajax/TestAjax.java
@@ -3,7 +3,7 @@ package tw.com.booked.ajax;
 public class TestAjax {
 
 	public static void main(String[] args) {
-		// TODO Auto-gadasdenerated method stub
+		// TODO Auto-gadasdsdfaenerated method stub
 
 	}
 
